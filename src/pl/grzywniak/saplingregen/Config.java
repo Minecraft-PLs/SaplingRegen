2a91324509aa6813187b4dd8659d48d39a67706d
xCraftRayX <dawid@grzywniak.pl>
2021-11-29 23:17:37 +0100

    SaplingRegen

---
+++ src/pl/grzywniak/saplingregen/Config.java
@@ -0,0 +1,203 @@
+package pl.grzywniak.saplingregen;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.WorldCreator;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class Config 
+{
+	final public static File directory = new File("plugins/SaplingRegen/");
+	final public static File locations = new File("plugins/SaplingRegen/Locations.yml");
+	final public static File config = new File("plugins/SaplingRegen/Config.yml");
+	
+	public static boolean messageAfterDestroyingSaplings;
+	public static String textOfThisMessage;
+	public static int timer;
+	
+	public void createDefaultFiles()
+	{
+		if(!directory.exists())
+		{
+			directory.mkdir();
+		}
+		if(!locations.exists())
+		{
+			try
+			{
+				locations.createNewFile();
+				
+				Writer writer = new BufferedWriter(new FileWriter(locations));
+	             
+	            writer.write("Locations:");
+	            writer.close();
+			}
+			catch(IOException e)
+			{
+				e.getMessage();
+			}
+		}
+		if(!config.exists())
+		{
+			try
+			{
+				config.createNewFile();
+				
+				Writer writer = new BufferedWriter(new FileWriter(config));
+	             
+				writer.write("Config:");                                                       ((BufferedWriter) writer).newLine();
+				writer.write("  #What time do you check the locations of the saplings?");      ((BufferedWriter) writer).newLine();
+                writer.write("  TimerInSec: 15");                  						       ((BufferedWriter) writer).newLine();
+                writer.write("  MessageAfterDestroyingSaplings: true");                        ((BufferedWriter) writer).newLine();
+                writer.write("  TextOfThisMessage: '§eRemember to plant the tree later ;)'");  ((BufferedWriter) writer).newLine();
+	            writer.close();
+			}
+			catch(IOException e)
+			{
+				e.getMessage();
+			}
+		}
+		getVariables();
+	}
+	
+	public static void getVariables()
+	{
+		FileConfiguration conf = YamlConfiguration.loadConfiguration(config);
+		
+		messageAfterDestroyingSaplings = conf.getBoolean("Config.MessageAfterDestroyingSaplings");
+		textOfThisMessage = conf.getString("Config.TextOfThisMessage");
+		timer = conf.getInt("Config.TimerInSec");
+	}
+	
+	public static HashMap<Integer, Location> getLocations()
+	{
+		HashMap<Integer, Location> locationss = new HashMap<Integer, Location>();
+		FileConfiguration conf = YamlConfiguration.loadConfiguration(locations);
+		
+		for(int i = 1; i <= getCountOfLocations(); i++)
+		{
+			if(conf.contains("Locations." + i + ".world"))
+			{
+				locationss.put(i, new Location(Bukkit.getServer().createWorld(new WorldCreator(conf.getString("Locations." + i + ".world"))), conf.getDouble("Locations."+i+".x"), conf.getDouble("Locations."+i+".y"), conf.getDouble("Locations."+i+".z")));
+			}
+		}
+		
+		return locationss;
+	}
+	
+	public void setLocation(int x, int y, int z, String worldName)
+	{
+		
+		int count = getCountOfLocations() + 1;
+		try
+		{
+			FileConfiguration location = YamlConfiguration.loadConfiguration(locations);
+			location.createSection("Locations." + count + ".world");
+			location.createSection("Locations." + count + ".x");
+			location.createSection("Locations." + count + ".y");
+			location.createSection("Locations." + count + ".z");
+			
+			location.set("Locations." + count + ".world", worldName);
+			location.set("Locations." + count + ".x", x);
+			location.set("Locations." + count + ".y", y);
+			location.set("Locations." + count + ".z", z);
+			
+			location.save(locations);
+			SaplingRegen.getInstance().saplingList.put(count, new Location(Bukkit.getWorld(worldName), x, y, z));
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void removeLocation(int number) 
+	{
+		int count = getCountOfLocations();
+		FileConfiguration conf = YamlConfiguration.loadConfiguration(locations);
+		try
+		{
+			conf.set("Locations." + number + ".world", conf.get("Locations." + count + ".world"));
+			conf.set("Locations." + number + ".x", conf.get("Locations." + count + ".x"));
+			conf.set("Locations." + number + ".y", conf.get("Locations." + count + ".y"));
+			conf.set("Locations." + number + ".z", conf.get("Locations." + count + ".z"));
+			conf.save(locations);
+			conf.set("Locations." + count, null);
+			conf.save(locations);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static int getCountOfLocations()
+	{
+		int count = 0;
+		
+		FileConfiguration location = YamlConfiguration.loadConfiguration(locations);
+		ConfigurationSection defined = location.getConfigurationSection("Locations");
+		
+		if(defined != null)
+		{
+//			for(String keys : defined.getKeys(false).size())
+//			{
+//				count += 1;
+//			}
+			count = defined.getKeys(false).size();
+		}
+		return count;
+	}
+
+	public boolean remove(Location location)
+	{
+		boolean isExist = false;
+		
+		if(location.getX() < 0)
+		{
+			location.setX(location.getX() - 1);
+		}
+		if(location.getZ() < 0)
+		{
+			location.setZ(location.getZ() - 1);
+		}
+		
+		for (Map.Entry<Integer, Location> lista : getLocations().entrySet())
+		{
+		    Integer key = lista.getKey();
+		    Location value = lista.getValue();
+		    
+		    if(value.equals(location))
+		    {
+		    	isExist = true;
+		    	removeLocation(key);
+		    	
+		    	break;
+		    }
+		}
+		
+		int tempKeyToRemove = -1;
+		
+        for (int key: SaplingRegen.getInstance().saplingList.keySet())
+        {
+        	if(SaplingRegen.getInstance().saplingList.get(key).equals(location))
+        	{
+        		tempKeyToRemove = key;
+        		break;
+        	}
+        }
+        if(tempKeyToRemove != -1) SaplingRegen.getInstance().saplingList.remove(tempKeyToRemove);
+		
+		return isExist;
+	}
+}
---
+++ src/pl/grzywniak/saplingregen/SaplingRegen.java
@@ -0,0 +1,177 @@
+package pl.grzywniak.saplingregen;
+
+import java.util.HashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SaplingRegen extends JavaPlugin implements Listener
+{
+	private static SaplingRegen instance;
+	private boolean warnOnBreakTreeIsEnabled;
+	private static Config cfg = new Config();
+	public HashMap<Integer, Location> saplingList = new HashMap<Integer, Location>();
+	
+	//TODO
+	//- Po scienciu drzewa odczekac ilosc cykli do ponownego zasadzenia
+	//- Blokada niszczenia sadzonek [CONFIG TRUE/FALSE]
+	//- Efekty na roznych wersjach mc
+	
+	@Override
+	public void onEnable()
+	{
+		int pluginID = 1953;
+        new Metrics(this, pluginID);
+		Bukkit.getPluginManager().registerEvents(this, this);
+		cfg.createDefaultFiles();
+		instance = this;
+		checkSapling();
+		saplingList = Config.getLocations();
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		System.out.println("Thanks for using my plugin from MineS.pl :)");
+	}
+	
+	public static SaplingRegen getInstance()
+	{
+		return instance;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if(cmd.getName().equalsIgnoreCase("sapling"))
+		{
+			if(sender instanceof Player)
+			{
+				Player p = (Player)sender;
+				if(p.hasPermission("SaplingRegen.use"))
+				{
+					if(args.length == 1)
+					{
+						if(args[0].equals("set"))
+						{
+							Location l = p.getLocation();
+	
+							int getX = (int) l.getX();
+							int getY = (int) l.getY();
+							int getZ = (int) l.getZ();
+							
+							String getWorldName = l.getWorld().getName();
+							
+							if(getX < 0)
+							{
+								getX -= 1;
+							}
+							if(getZ < 0)
+							{
+								getZ -= 1;
+							}
+							
+							l.setX(getX);
+							l.setY(getY);
+							l.setZ(getZ);
+							l.setPitch(0);
+							l.setYaw(0);
+
+							if(!saplingList.containsValue(l)) //PLAYER ZAOKRAGLIC
+							{
+								p.sendMessage("§aYou have set up regenerating saplings!");
+								cfg.setLocation(getX, getY, getZ, getWorldName);
+							}
+							else
+							{
+								p.sendMessage("§cThis place has already been set!");
+							}
+						}
+						else if(args[0].equals("remove"))
+						{
+							Location pLoc = new Location(p.getLocation().getWorld(), (int) p.getLocation().getX(), (int) p.getLocation().getY(), (int) p.getLocation().getZ());
+							
+							if(cfg.remove(pLoc))
+							{
+								p.sendMessage("§aLocation was removed!");
+							}
+							else
+							{
+								p.sendMessage("§cDoes not exist sapling at this location!");
+							}
+						}
+						else
+						{
+							p.sendMessage("§cBad command!\n§eTry use: /sapling <set, remove>");
+						}
+					}
+					else
+					{			
+						p.sendMessage("§cBad command!\n§eTry use: /sapling <set, remove>");
+					}
+				}
+				else
+				{
+					p.sendMessage("§cYou have not permission §e'saplingregen.use'");
+				}
+			}
+			else
+			{
+				sender.sendMessage("§cYou must be a player!");
+			}
+			
+		}
+		return false;
+	}
+	
+	public void checkSapling()
+	{
+		Bukkit.getScheduler().runTaskTimer(getInstance(), new Runnable()
+		{
+            @Override
+			public void run()
+            {
+				for(int i = 1; i <= saplingList.size(); i++)
+				{
+					Location loc = saplingList.get(i);
+					if(loc.getBlock().isEmpty())
+					{
+						loc.getBlock().setType(Material.OAK_SAPLING);
+						if(!Bukkit.getOnlinePlayers().isEmpty())
+						{
+							for(Player p : Bukkit.getOnlinePlayers())
+							{
+								if(p.getLocation().getWorld().equals(loc.getWorld()) && p.getLocation().distance(loc) < 16)
+								{
+									p.spawnParticle(Particle.CLOUD, loc, 50);
+//									PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.CLOUD, false, (float)loc.getX(), (float)loc.getY() + 1.0F, (float)loc.getZ(), 0.2F, 0.5F, 0.2F, 0.1F, 200, null);
+//									((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+									p.playSound(loc, Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 0.5f, 1f);
+								}
+							}
+						}
+					}
+				}
+            }
+		}, 0, 20 * Config.timer);
+	}
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent e)
+	{
+		if(warnOnBreakTreeIsEnabled && e.getBlock().getType().toString().contains("SAPLING") && !e.getBlock().getType().toString().contains("POTTED"))
+		{
+			e.getPlayer().sendMessage(Config.textOfThisMessage);
+		}
+	}
+}
